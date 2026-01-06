b06328000c03189e61ce79f9ce84e10cb0ddcd3c
xuu <chochimalupa@gmail.com>
2026-01-06 19:59:38 +0800

    refactor-變更資料夾位置

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 
 import './index.css'
 import App from './App.jsx'
-import './assets/scss/all.scss'
+import './style/all.scss'
 import 'bootstrap'
 
 createRoot(document.getElementById('root')).render(
