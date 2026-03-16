9e1f0232b107a887488b352d992a385bb95958d2
chong <c22425688@gmail.com>
2026-03-17 01:28:48 +0800

    header login 修改

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from '../slice/cartSlice';
+import authReducer from '../slice/authSlice'; 
 
-export const store=configureStore({
-    reducer :{
-        cart:cartReducer,
+export const store = configureStore({
+    reducer: {
+        cart: cartReducer,
+        auth: authReducer, 
     },
 });
