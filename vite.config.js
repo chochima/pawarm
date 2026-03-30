c907ddbbc31ba10569a4e83663b623c0ced45d32
chong <c22425688@gmail.com>
2026-03-31 00:38:21 +0800

    3/30 修改

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
 
-// 🚩 修改點：使用 command 或 mode 來判斷
+
 export default defineConfig(({ mode }) => {
   return {
-    // 如果 repo 名稱叫 pawarm，請確保前後都有斜線
+  
     base: mode === 'production' ? '/pawarm/' : '/',
     plugins: [
       react(),
